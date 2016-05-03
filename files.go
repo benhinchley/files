e24012e6da921e6e05857a9fa66b4442fff851cd
Benjamin Hinchley <benjaminhinchley@gmail.com>
2016-05-03 19:18:40 +1000

    add initial implementation
    adds Exists, StripRoot, and ListPath
    
    Signed-off-by: Benjamin Hinchley <benjaminhinchley@gmail.com>

---
+++ files.go
@@ -0,0 +1,51 @@
+package files
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/benhinchley/log"
+)
+
+// Exists checks whether the given path exists.
+func Exists(p string) bool {
+	_, err := os.Stat(p)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// StripRoot strips the root of the path off.
+func StripRoot(root, p string) string {
+	if !strings.HasSuffix(root, "/") {
+		root = root + "/"
+	}
+	return strings.TrimPrefix(p, root)
+}
+
+// ListPath lists the contents of a given path.
+func ListPath(p string) <-chan string {
+	w := make(fileWalk)
+	go func() {
+		if err := filepath.Walk(p, w.Walk); err != nil {
+			close(w)
+			log.Fatal(err)
+		}
+		close(w)
+	}()
+	return w
+}
+
+type fileWalk chan string
+
+func (f fileWalk) Walk(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() {
+		f <- path
+	}
+	return nil
+}
